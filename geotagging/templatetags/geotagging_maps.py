b6c961a94f1d5fd39a58bb91ffa7de93a480c0ef
Nicolas Lara <nicolaslara@gmail.com>
2011-03-26 20:01:54 +0100

    added missing ,

--- geotagging/templatetags/geotagging_maps.py
+++ geotagging/templatetags/geotagging_maps.py
@@ -19,8 +19,8 @@ class Map(InclusionTag):
     options = Options(
         Argument('id'),
         Argument('latlng', default='55.6845043579,12.5735950447'),
-        Argument('width', default='300')
-        Argument('height', default='400')
+        Argument('width', default='300'),
+        Argument('height', default='400'),
         Argument('zoom', default='16')
     )
 
