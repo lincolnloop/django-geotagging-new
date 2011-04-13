1e4c19441f19e37818135431eab276c1321e6002
Nicolas Lara <nicolaslara@gmail.com>
2011-04-13 12:07:45 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-geotagging-new',
-    version='0.1.2',
+    version='0.1.4',
     description=('This is a geotagging application. '
                  'It can be used to localize your content.'),
     author='Nicolas Lara',
